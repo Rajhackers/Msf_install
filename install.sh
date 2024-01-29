a97d9500d366c0aa827f7e00c635287b64bb2f71
Raj hackers <153776272+Rajhackers@users.noreply.github.com>
2024-01-29 06:19:33 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,8 +9,8 @@ echo " # copy and paste # "
 echo "
 apt update -y
 yes | apt upgrade 
-pkg install curl -y
-apt-get install gnupg -y
+apt install curl 
+apt-get install gnupg 
 
 curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && \
 chmod 755 msfinstall && \
